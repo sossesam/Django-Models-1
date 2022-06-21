d9d4f67de935e2445f5b9404bc04ac9179d5e0d1
oyeniran samuel sanmi <sossesam@gmail.com>
2022-06-21 08:00:44 +0100

    created blog

--- I4G0071527SP/blog/models.py
+++ I4G0071527SP/blog/models.py
@@ -1,3 +1,15 @@
+
 from django.db import models
 
+
 # Create your models here.
+class Author:
+    userName=models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+  
+    Created_date= models.DateTimeField(auto_now_add=True)
+    Published_date= models.DateTimeField(auto_now_add=True)
