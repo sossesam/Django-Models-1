6c686d9bb3b32b5e7eef4901fb05c13a78de800a
oyeniran samuel sanmi <sossesam@gmail.com>
2022-06-21 09:06:30 +0100

    sorted out the migration

--- I4G0071527SP/blog/models.py
+++ I4G0071527SP/blog/models.py
@@ -1,15 +1,17 @@
 
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Author:
-    userName=models.CharField(max_length=50)
 
 
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-  
+    Author= models.ForeignKey(User, on_delete=models.CASCADE, default='Anonymous')
     Created_date= models.DateTimeField(auto_now_add=True)
     Published_date= models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.Title
